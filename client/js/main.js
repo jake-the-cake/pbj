c9311720f19b67d147154d18430f89bb01c24878
Jakey Wakey <askjake331@gmail.com>
2023-10-24 21:22:22 -0400

    updates

--- client/js/main.js
+++ client/js/main.js
@@ -1,6 +1,18 @@
 window.addEventListener('DOMContentLoaded', () => {
-	relocateUnderNavbar()
+	useHiddenNavigation('hidden-nav')
+	
+	
+	// relocateUnderNavbar()
 })
+
+function useHiddenNavigation(id) {
+	const hiddenNavbar = document.getElementById(id)
+	hiddenNavbar.style.top = '-' + hiddenNavbar.offsetHeight + 'px'
+	window.addEventListener('scroll', (e) => {
+		if (window.scrollY > 30 && !hiddenNavbar.classList.contains('top-0')) hiddenNavbar.classList.add('top-0')
+		if (window.scrollY <= 30 && hiddenNavbar.classList.contains('top-0')) hiddenNavbar.classList.remove('top-0')
+	})
+}
 
 function relocateUnderNavbar(spaceInPx = 16) {
 	const navigationHeight = document.querySelector('nav')?.offsetHeight || null
